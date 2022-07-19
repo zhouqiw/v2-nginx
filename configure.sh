814408ce50c41ed736df871332008b5d19a8ab72
qi <22001638+zhouqiw@users.noreply.github.com>
2022-07-19 22:19:39 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -18,6 +18,7 @@ cd /opt/test
 dd if=/dev/zero of=100mb.bin bs=100M count=1
 dd if=/dev/zero of=10mb.bin bs=10M count=1
 # Run V2Ray
+cat /usr/local/etc/v2ray/config.json
 /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json &
 # Run nginx
 /bin/bash -c "envsubst '\$PORT,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
